12bef45ad8126684f233aee7f228bdb81c990bd4
Jeff Garner <jeff.garner@shelbyinc.com>
2015-12-31 10:53:54 -0600

    Adding Calculate Method.

---
+++ Playground/Playground/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyObject myObject = new MyObject();
+
+            myObject.number1 = 10;
+            myObject.number2 = 5;
+
+            Console.WriteLine("Number 1 is " + myObject.number1);
+            Console.WriteLine("Number 2 is " + myObject.number2);
+            Console.WriteLine("The Calculation is " + myObject.Calculate());
+            Console.ReadLine();
+        }
+    }
+
+    class MyObject
+    {
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+
+        public int Calculate()
+        {
+            return number1 + number2;
+        }
+        public string MyMethod()
+        {
+            return "some value";
+        }
+    }
+}
