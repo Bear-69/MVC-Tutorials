ec8fc75b0e647cf610ab1dd875fffdd9b0b4d4ec
Jeff Garner <jeff.garner@shelbyinc.com>
2015-12-31 12:36:46 -0600

    Made changes to the MyObject method.

--- Playground/Playground/Program.cs
+++ Playground/Playground/Program.cs
@@ -12,22 +12,22 @@ namespace Playground
         {
             MyObject myObject = new MyObject();
 
-            myObject.number1 = 10;
-            myObject.number2 = 5;
+            //myObject.number1 = 10;
+            //myObject.number2 = 5;
 
-            Console.WriteLine("Number 1 is " + myObject.number1);
-            Console.WriteLine("Number 2 is " + myObject.number2);
-            Console.WriteLine("The Calculation is " + myObject.Calculate());
+            //Console.WriteLine("Number 1 is " + myObject.number1);
+            //Console.WriteLine("Number 2 is " + myObject.number2);
+            Console.WriteLine("The Calculation is " + myObject.Calculate(10, 5));
             Console.ReadLine();
         }
     }
 
     class MyObject
     {
-        public int number1 { get; set; }
-        public int number2 { get; set; }
+        //public int number1 { get; set; }
+        //public int number2 { get; set; }
 
-        public int Calculate()
+        public int Calculate(int number1, int number2)
         {
             return number1 + number2;
         }
