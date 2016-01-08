b1d6578df014ca15187df5a84e9dff934b1c1ac7
Jeff Garner <jeff.garner@shelbyinc.com>
2016-01-08 15:39:53 -0600

    Lecture 45: Code Base downloaded to match instructors.

--- eCommerce/eCommerce.Contracts/Properties/AssemblyInfo.cs
+++ eCommerce/eCommerce.Contracts/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("eCommerce.Contracts")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2bea8873-da24-423e-abbf-c0aaff6ffe40")]
+[assembly: Guid("d7a63100-f2d8-47d7-bf98-912d19471dd0")]
 
 // Version information for an assembly consists of the following four values:
 //
--- eCommerce/eCommerce.DAL/Properties/AssemblyInfo.cs
+++ eCommerce/eCommerce.DAL/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("eCommerce.DAL")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("804b5bab-0660-4da9-9ba2-a5b1a78e6e25")]
+[assembly: Guid("fe744344-870c-4c29-aa07-12258df8bca2")]
 
 // Version information for an assembly consists of the following four values:
 //
