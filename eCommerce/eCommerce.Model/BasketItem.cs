33f3da9d0a7f361a9df6579bb18e1b4e09c7819a
Jeff Garner <jeff.garner@shelbyinc.com>
2016-01-11 17:01:33 -0600

    Lecture 49: Code So Far - Code Update to ensure code matches instructor.

--- eCommerce/eCommerce.Model/BasketItem.cs
+++ eCommerce/eCommerce.Model/BasketItem.cs
@@ -7,16 +7,12 @@ using System.Threading.Tasks;
 
 namespace eCommerce.Model
 {
-    public class Product
+    public class BasketItem
     {
+        public int BasketItemId { get; set; }
+        public int BasketId { get; set; }
         public int ProductId { get; set; }
 
-        public string Description { get; set; }
-
-        [MaxLength(255)]
-        public string ImageUrl { get; set; }
-
-        public decimal Price { get; set; }
-        public decimal CostPrice { get; set; }
+        public Product Product { get; set; }
     }
 }
